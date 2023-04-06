41c6ab69e09ac2901d2d7ee30491f2a250134336
rlkeen13 <rlkeen13@gmail.com>
2023-04-06 09:06:42 -0400

    Update CU_config.js

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2023vagle",
+      "defaultValue": "2023chcmp",
       "required": "true",
       "disabled": "true"
     },
